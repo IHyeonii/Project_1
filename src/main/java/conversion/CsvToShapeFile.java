8c6e30d8c163bfdebcb32e86141f7063e5e200cd
IHyeon <dlgusdllll@gmail.com>
2023-09-24 21:30:32 +0900

    GeoTools_Create points in QGIS

--- src/main/java/conversion/CsvToShapeFile.java
+++ src/main/java/conversion/CsvToShapeFile.java
@@ -1,5 +1,6 @@
-package tutorial;
+package conversion;
 
+import com.opencsv.CSVReader;
 import org.geotools.api.data.SimpleFeatureSource;
 import org.geotools.api.data.SimpleFeatureStore;
 import org.geotools.api.data.Transaction;
@@ -16,7 +17,7 @@ import org.geotools.geometry.jts.JTSFactoryFinder;
 import org.geotools.swing.data.JFileDataStoreChooser;
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.GeometryFactory;
-import org.locationtech.jts.geom.Point;
+import org.locationtech.jts.geom.LineString;
 
 import javax.swing.*;
 import java.io.BufferedReader;
@@ -28,11 +29,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * This example reads data for point locations and associated attributes from a comma separated text (CSV) file
- * and exports them as a new shapefile.
- * */
-public class Csv2Shape {
+// 공백을 콤마로 대체해서 넣기
+// openCSV 안 써도 될거같아
+
+public class CsvToShapeFile {
   public static void main(String[] args) throws Exception {
     UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
 
@@ -40,21 +40,14 @@ public class Csv2Shape {
     if (file == null) {
       return;
     }
-
     // 1. FeatureType 생성
-    // To describe the data that we are importing from the CSV file and writing to a shapefile.
+    // SimpleFeatureType 생성 =  CSV 파일에서 읽어온 데이터를 설명, 속성 유형과 구조 정의
     final SimpleFeatureType TYPE =
         DataUtilities.createType( // DataUtilities 사용
-            "Link",
+            "Location",
             "the_geom:Point:srid=4326,"
-                + // <- the geometry attribute: Point type
-                "name:String,"
-                + // <- a String attribute
-                "number:Integer" // a number attribute
         );
     System.out.println("TYPE:" + TYPE);
-    // QuickStart 실행 -> csv파일 선택하면
-    // SimpleFeatureTypeImpl 위치 식별하면 -> Feature(the_geom:the_geom,name:name,number:number) 확장 됨
 
     // 2. Features 생성 -> Point, Line(점들의 연결), Polygon, Attribute..
     // 이제 CSV 파일을 읽고 각 줄을 파싱하여 데이터 추출하기
@@ -67,47 +60,64 @@ public class Csv2Shape {
     // 2-2. SimpleFeatureBuilder 사용하여 Features(SimpleFeature 객체) 생성
     SimpleFeatureBuilder featureBuilder = new SimpleFeatureBuilder(TYPE);
 
-    try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+    try {
+      BufferedReader reader = new BufferedReader(new FileReader(file));
+      CSVReader csvReader = new CSVReader(reader);
+
       /* First line of the data file is the header */
-      String line = reader.readLine();
+      String[] line = csvReader.readNext();
       System.out.println("Header: " + line);
 
-      for (line = reader.readLine(); line != null; line = reader.readLine()) {
-        if (line.trim().length() > 0) { // skip blank lines -> isEmpty로 해보기
-          String[] tokens = line.split("\\,"); // 쉼표(,)로 구분된 데이터 파싱
+      while ((line = csvReader.readNext()) != null) {
 
-          // cvs 파일 각 세로 값들
-          double latitude = Double.parseDouble(tokens[0]);
-          double longitude = Double.parseDouble(tokens[1]);
-          String name = tokens[2].trim(); // CITY 값을 문자열로 추출하고 양 끝의 공백 제거 -> 이게 dbf 파일로 넘어감
-          int number = Integer.parseInt(tokens[3].trim());
+        String lineString = line[14];
 
-          /* Longitude (= x coord) first ! */
-          // 위도와 경도를 사용하여 Point 객체를 생성
-          Point point = geometryFactory.createPoint(new Coordinate(longitude, latitude));
+        int start = lineString.indexOf('(');
+        int end = lineString.indexOf(')');
+        String result = lineString.substring(start+1, end); // 괄호 안의 숫자만 출력
 
-          // featureBuilder 객체를 사용하여 point 객체랑 name, number -> feature에 추가
-          featureBuilder.add(point);
-          featureBuilder.add(name);
-          featureBuilder.add(number);
+        // 콤마 또는 공백을 구분자로 분리
+        String[] tokens = result.split(",| ");
 
-          // featureBuilder 사용하여 SimpleFeature 객체 생성 -> 여기에 모든 데이터가 포함 됨
-          SimpleFeature feature = featureBuilder.buildFeature(null); // null 대신 feature의 ID나 식별자 지정 가능
+        System.out.println(tokens[0]);
+        System.out.println(tokens[1]);
 
-          // 생성한 feature를 features 리스트에 추가
-          features.add(feature);
-        }
+        // cvs 파일 각 세로 값들
+        double stLongitude = Double.parseDouble(tokens[0]); // 경도
+        double stLatitude = Double.parseDouble(tokens[1]); // 위도
+        double endLongitude = Double.parseDouble(tokens[tokens.length-2]); // 경도 끝값
+        double endLatitude = Double.parseDouble(tokens[tokens.length-1]); // 위도 끝값
+
+        /* Longitude (= x coord) first ! */
+        // 위도와 경도를 사용하여 Point 객체를 생성
+        // 두 개의 Point 생성하여 LineString 만들기
+        Coordinate startCoord = new Coordinate(stLongitude, stLatitude); // 시작점
+        Coordinate endCoord = new Coordinate(endLongitude, endLatitude); // 끝점
+
+        // LineString 생성
+        LineString lineInfo = geometryFactory.createLineString(new Coordinate[]{startCoord, endCoord});
+
+        // featureBuilder 객체를 사용하여 point 객체랑 name, number -> feature에 추가
+        featureBuilder.add(lineInfo);
+
+        // featureBuilder 사용하여 SimpleFeature 객체 생성 -> 여기에 모든 데이터가 포함 됨
+        SimpleFeature feature = featureBuilder.buildFeature(null);
+
+        // 생성한 feature를 features 리스트에 추가
+        features.add(feature);
       }
+    } catch (Exception e) {
+      e.printStackTrace();
     }
 
     // 3. Create a shapefile From a FeatureCollection
     // getNewShapeFile 메서드 호츌
-    File newFile = getNewShapeFile(file); // 39번 라인 -> 출력할 파일(csv) name 가져오기
+    File newFile = getNewShapeFile(file); // 읽어온 csv 파일 가져와서
 
     // 3-1.DataStoreFactory -> 원하는 공간 인덱스를 표시하는 매개변수와 함께 사용해야 함
     ShapefileDataStoreFactory dataStoreFactory = new ShapefileDataStoreFactory();
 
-    Map<String, Serializable> params = new HashMap<>(); // 직렬화 ?
+    Map<String, Serializable> params = new HashMap<>();
     params.put("url", newFile.toURI().toURL());
     params.put("create spatial index", Boolean.TRUE);
 
@@ -123,13 +133,8 @@ public class Csv2Shape {
     String typeName = newDataStore.getTypeNames()[0];
     SimpleFeatureSource featureSource = newDataStore.getFeatureSource(typeName);
     SimpleFeatureType SHAPE_TYPE = featureSource.getSchema();
-    /*
-     * The Shapefile format has a couple limitations:
-     * "the_geom" is always first, and used for the geometry attribute name
-     * "the_geom" must be of type Point, MultiPoint, MuiltiLineString, MultiPolygon
-     * Attribute names are limited in length
-     */
-    System.out.println("SHAPE:" + SHAPE_TYPE);
+
+    System.out.println("SHAPE:" + SHAPE_TYPE); // 타입이 Point로 넘어와..
 
     if (featureSource instanceof SimpleFeatureStore) {
       SimpleFeatureStore featureStore = (SimpleFeatureStore) featureSource;
