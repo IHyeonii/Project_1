a61ffe7bfceed4f801817efba6cc65561c504dd7
IHyeon <dlgusdllll@gmail.com>
2023-09-30 17:50:45 +0900

    create CRSLab: ...

--- src/main/java/completion/CRSLab.java
+++ src/main/java/completion/CRSLab.java
@@ -21,14 +21,12 @@ import java.util.Map;
 
 public class CRSLab { //좌표는 변경됐는데 값이 안들어감 ㅋ
   public static void main(String[] args) throws Exception {
+    File inputFile = new File("C:\\Users\\ihyeon\\Desktop\\FirstTask\\Node_test.shp");
 
-    File inputShpFile = new File("C:\\Users\\ihyeon\\Desktop\\FirstTask\\Node_test.shp");
-    File outputShpFile = new File("C:\\Users\\ihyeon\\Desktop\\FirstTask\\changeCRS\\convertNode.shp");
-
-    // FileDataStore -> shp 로드, featureSource 구하기
-    FileDataStore dataStore = FileDataStoreFinder.getDataStore(inputShpFile);
-    SimpleFeatureSource featureSource = dataStore.getFeatureSource();
-    SimpleFeatureCollection featureCollection = featureSource.getFeatures();
+    // FileDataStore -> shp 파일 읽고 featureSource 구하기
+    FileDataStore dataStore = FileDataStoreFinder.getDataStore(inputFile);
+    SimpleFeatureSource featureSource = dataStore.getFeatureSource(); //FeatureSource -> 지리공간 정보를 Java 객체로 액세스 가능
+    SimpleFeatureType schema = featureSource.getSchema(); // 원본 shp파일 스키마 가져와서
 
     // 좌표체계 정의
     CoordinateReferenceSystem sourceCRS = CRS.decode("EPSG:4162");
@@ -40,25 +38,31 @@ public class CRSLab { //좌표는 변경됐는데 값이 안들어감 ㅋ
     // 좌표체계 변환
     MathTransform transform = CRS.findMathTransform(sourceCRS, targetCRS, lenient);
 
-    // Define the schema for the output Shapefile
+    //FeatureCollection = JDBC와 유사, 메모리 연결, Iterator 꼭 닫으래
+    SimpleFeatureCollection featureCollection = featureSource.getFeatures();
 
-    SimpleFeatureType schema = featureSource.getSchema(); // 입력 shp파일 스키마 가져와서
+    // SimpleFeatureBuilder = Creation of features (SimpleFeature objects)
     SimpleFeatureTypeBuilder featureTypeBuilder = new SimpleFeatureTypeBuilder();
     featureTypeBuilder.init(schema);
-    featureTypeBuilder.setCRS(targetCRS);
+    featureTypeBuilder.setCRS(targetCRS); //출력 파일의 스키마 설정을 위해 필요
 
-    // Create a new DataStore for the output Shapefile
+    /*
+     * Get an output file name and create the new shapefile
+     */
+    File newFile = getNewShapeFile(inputFile);
+
     DataStoreFactorySpi factory = new ShapefileDataStoreFactory();
-    Map<String, Serializable> create = new HashMap<>();
-    create.put("url", outputShpFile.toURI().toURL()); // 이 부분이 애매해
-    create.put("create spatial index", Boolean.TRUE);
-    DataStore newDataStore = factory.createNewDataStore(create);
+    Map<String, Serializable> params = new HashMap<>();
+    params.put("url", newFile.toURI().toURL());
+    params.put("create spatial index", Boolean.TRUE);
+    DataStore newDataStore = factory.createNewDataStore(params);
     SimpleFeatureType featureType = SimpleFeatureTypeBuilder.retype(schema, targetCRS);
-    newDataStore.createSchema(featureType);
+
 
     // Get the name of the new Shapefile
     // 근데, newDataStore 기져오면 convertNode 존재하지 않는데
     String typeName = dataStore.getTypeNames()[0];
+    newDataStore.createSchema(featureType);
 
     Transaction transaction = new DefaultTransaction("Reproject");
     try (FeatureWriter<SimpleFeatureType, SimpleFeature> writer =
@@ -84,5 +88,16 @@ public class CRSLab { //좌표는 변경됐는데 값이 안들어감 ㅋ
     } finally {
       transaction.close();
     }
+  }
+
+  private static File getNewShapeFile(File csvFile) {
+    File newFile = new File("C:\\Users\\ihyeon\\Desktop\\FirstTask\\changeCRS\\convertNode.shp");
+
+    // 새로운 파일이 원본 CSV 파일과 동일한 경우
+    if (newFile.equals(csvFile)) {
+      System.out.println("Error: cannot replace " + csvFile);
+      System.exit(0);
+    }
+    return newFile;
   }
 }
